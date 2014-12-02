251c1808ca251ea3b8990a1705bb4c58a900f30f
Kristiyan Yordanov <kristiyanjordanov@gmail.com>
2014-12-02 19:52:30 +0200

    Removed black rows.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,9 @@
 using namespace std;
 #define CLEAR_FILE 1
 string userInputContent();
-
 string userInputFileName();
-
 void writeInFile(string fileName, string content);
-
 void readFile(string fileName);
-
 void clearFile(string fileName);
 
 int main()
